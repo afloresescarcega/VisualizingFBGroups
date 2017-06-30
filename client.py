c5b8c45b873d209aa5f1d0e7cca113067bf10fc0
Alex Flores <mirnaescarcega@Alexs-iMac.local>
2017-06-29 21:05:39 -0500

    Add method to return info to be displayed for the whole page of app

--- client.py
+++ client.py
@@ -14,6 +14,26 @@ graph = GraphAPI(tokenAndExpiration[0])
 # Hardcoded UT FB meme group
 # graph.get('1218486471522469/posts')
 
-print(graph.get('1218486471522469_1378570272180754?fields=reactions'))
-print(graph)
+"""
+Returns the name of the group, the icon picture url, the cover picture url
+Parameters: FBGroup type int : the facebook group id
+"""
+def getFBGroupInfo(FBGroup):
+    assert(type(FBGroup) is type(int())) # Ensure that preconditions hold
+    groupGraph = graph.get(str(FBGroup) + "?fields=icon,cover,name")
+    name = groupGraph["name"]
+    iconUrl = groupGraph["icon"]
+    coverUrl = groupGraph["cover"]["source"]
+    return (name, iconUrl, coverUrl)
+
+# print(graph.get('1218486471522469_1378570272180754?fields=reactions'))
+# print(graph)
+
+# test group info
+group_info = str(getFBGroupInfo(1218486471522469))
+print(type(group_info))
+print(group_info)
+
+
+
 
